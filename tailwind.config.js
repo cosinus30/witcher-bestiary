3d3837e5d9864d265c18ca4e681e0d7cdb4ab09c
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-04-16 12:04:29 +0300

    style: extend tailwind defaults

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darky: "#202125"
+      },
+      dropShadow: {
+        'darky': '0 8px 8px rgba(0, 0, 0, 0.25)'
+      },
+      fontFamily: {
+        'cinzel': ['Cinzel', 'serif']
+      }
+    },
   },
   plugins: [],
 }
