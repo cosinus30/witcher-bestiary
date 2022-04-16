398cf79cefbbad6f30b6414ca6204e69e7c0d4b0
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-04-16 13:16:06 +0300

    style: add bg image as tailwind utility

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
       },
       fontFamily: {
         'cinzel': ['Cinzel', 'serif']
+      },
+      backgroundImage: {
+        'witcher-image': "url('/witcher.png')"
       }
     },
   },
