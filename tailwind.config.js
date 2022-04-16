50baacb9a34f1bb1f5c319edfae1eb4e21c65e5f
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-04-16 17:39:20 +0300

    chore: add aspect ratio plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         darky: "#202125",
-        secondary: "#464F73"
+        secondary: "#464F73",
       },
       dropShadow: {
-        'darky': '0 8px 8px rgba(0, 0, 0, 0.25)'
+        darky: "0 8px 8px rgba(0, 0, 0, 0.25)",
       },
       fontFamily: {
-        'cinzel': ['Cinzel', 'serif']
+        cinzel: ["Cinzel", "serif"],
       },
       backgroundImage: {
-        'witcher-image': "url('/witcher.png')"
-      }
+        "witcher-image": "url('/witcher.png')",
+      },
     },
   },
-  plugins: [],
-}
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [require("@tailwindcss/aspect-ratio")],
+};
