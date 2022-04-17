13493770b97f0964b59040cd1fa11c4dc651c14b
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-04-17 13:23:23 +0300

    refactor: remove dummy data from filters

--- components/HomePage/MainBar.tsx
+++ components/HomePage/MainBar.tsx
@@ -2,52 +2,11 @@ import Image from "next/image";
 import Dropdown from "./Dropdown";
 import Filter from "./Filter";
 import Search from "./Search";
-interface FilterProps {
-  onFilterChange: (filter: string) => void;
-  filters: {
-    label: string;
-    value: string;
-    icon: React.ReactNode;
-  }[];
-}
-
-const filters = [
-  {
-    label: "Aard",
-    value: "aard",
-    icon: <Image src="/aard.svg" alt="aard sign" width={32} height={32}></Image>,
-    selected: false,
-  },
-  {
-    label: "Igni",
-    value: "igni",
-    icon: <Image src="/igni.svg" alt="igni sign" width={32} height={32}></Image>,
-    selected: false,
-  },
-  {
-    label: "Yrden",
-    value: "yrden",
-    icon: <Image src="/yrden.svg" alt="yrden sign" width={32} height={32}></Image>,
-    selected: false,
-  },
-  {
-    label: "Axii",
-    value: "axii",
-    icon: <Image src="/axii.svg" alt="axii sign" width={32} height={32}></Image>,
-    selected: false,
-  },
-  {
-    label: "Quen",
-    value: "quen",
-    icon: <Image src="/quen.svg" alt="quen sign" width={32} height={32}></Image>,
-    selected: true,
-  },
-];
 
 const MainBar = () => {
   return (
     <div className="flex flex-col justify-center my-8 mx-auto border border-secondary/75 md:flex-row md:items-center md:justify-between md:max-w-5xl p-4">
-      <Filter filters={filters} />
+      <Filter />
       <Search />
       <Dropdown /> 
     </div>
