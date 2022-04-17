b0e31567249dbdf2430359040fe1bea6b74786a0
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-04-17 16:38:35 +0300

    chore: address accessibility issues

--- components/Navigation/Navbar.tsx
+++ components/Navigation/Navbar.tsx
@@ -4,21 +4,21 @@ import NavbarItem from "./NavbarItem";
 
 const Navbar = () => {
   return (
-    <header className="mb-8 drop-shadow-darky">
+    <header className="mb-8 shadow-lg">
       <div className="flex justify-between items-center p-6">
         <Link href="/">
-          <a className="font-cinzel text-4xl font-semibold text-white">Bestiary</a>
+          <a className="font-cinzel text-3xl font-semibold text-white sm:text-4xl">Bestiary</a>
         </Link>
         <nav className="flex space-x-2">
           <NavbarItem
             label="Github"
             link="https:www.github.com/cosinus30/witcher-bestiary"
-            logo={<Image src="/icons8-github.svg" height={30} width={30} />}
+            logo={<Image src="/icons8-github.svg" height={30} width={30} alt="Github link" />}
           />
           <NavbarItem
             label="Figma"
             link="https://www.figma.com/file/9h2dYAYnF9tQOfxp9EO0GS/Witcher-3?node-id=3%3A226"
-            logo={<Image src="/icons8-figma.svg" height={30} width={30} />}
+            logo={<Image src="/icons8-figma.svg" height={30} width={30} alt="Figma link"/>}
           />
         </nav>
       </div>
