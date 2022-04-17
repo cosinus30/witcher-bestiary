f64c91ff16c418b8a853ba727a21e90974fc36ad
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-04-17 16:37:38 +0300

    feat: add quote component to show blockquotes

---
+++ components/CardDetail/Quote.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface IQoute {
+  content: string;
+  author: string;
+}
+
+const Quote = ({ author, content }: IQoute) => (
+  <blockquote className="relative p-4 text-xl italic border-l-4 border-neutral-500">
+    <div className="" aria-hidden="true">
+      &ldquo;
+    </div>
+    <p className="mb-4 font-lustria text-base sm:text-lg">{content}</p>
+    <cite className="flex items-center">
+      <div className="flex flex-col items-start">
+        <span className="mb-1 text-sm italic font-regular">{author}</span>
+      </div>
+    </cite>
+  </blockquote>
+);
+
+export default Quote;
