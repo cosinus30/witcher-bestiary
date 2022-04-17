16f8f779c1f6569ae9a5715da00dd89350db9c16
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-04-17 14:27:00 +0300

    fix: image not found

--- components/HomePage/Card.tsx
+++ components/HomePage/Card.tsx
@@ -32,7 +32,7 @@ const Card = ({ type, image, name, weaknesses }: CardProps) => {
           <div className="flex absolute inset-x-0 bottom-0 justify-center items-center text-white bg-darky shadow-inner drop-shadow-lg">
             <span>{name}</span>
             <span className="pt-1 ml-2">
-              <Image src={`/${type}s.svg`} alt={type} width={32} height={32}></Image>
+              <Image src={`/${type}.svg`} alt={type} width={32} height={32}></Image>
             </span>
           </div>
       </div>
