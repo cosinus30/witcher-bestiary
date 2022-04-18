ffb93191a6ba7ddb4f1cecad52c1d9bb35c955dc
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-04-18 17:46:11 +0300

    feat: add data source for creature detail page Used staticProps since it is not a dynamic data

--- pages/[creatureName]/index.tsx
+++ pages/[creatureName]/index.tsx
@@ -1,7 +1,8 @@
-import type { NextPage } from "next";
+import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import Image from "next/image";
 import { useContext, useEffect, useState } from "react";
 import Quote from "../../components/CardDetail/Quote";
+import { supabase } from "../../util/SupabaseClient";
 
 interface IDetailPage {
   id: string;
@@ -21,11 +22,10 @@ interface IDetailPage {
   combatTactics: string[];
 }
 
-const Home: NextPage = () => {
-  const [creatureDetail, setCreatureDetail] = useState<IDetailPage>(cardDetil);
+const Home = (props: IDetailPage) => {
   const [selectedTab, setSelectedTab] = useState("bestiaryEntry");
+  const { name, type, images, bestiaryEntry, combatTactics, shortDescription, summary, weaknesses } = props;
 
-  const { name, type, images, bestiaryEntry, combatTactics, shortDescription, summary, weaknesses } = creatureDetail;
   return (
     <div className="mx-auto max-w-4xl">
       <h1 className="block ml-2 text-7xl sm:text-8xl ">{name}</h1>
@@ -40,7 +40,7 @@ const Home: NextPage = () => {
           <Image src={images[2].url} alt={images[2].alt} layout="fill" objectFit="cover"></Image>
         </div>
       </div>
-      <div className="grid gap-2 p-8 my-8 border border-secondary/75 divide-y divide-secondary/75 sm:grid-cols-2 sm:divide-x">
+      <div className="grid gap-2 p-8 my-8 border border-secondary/75 divide-secondary/75 sm:grid-cols-2 sm:divide-y-0 sm:divide-x">
         <div className="flex flex-col justify-center mr-16 space-y-4">
           <div className="flex justify-between items-center sm:grid sm:grid-cols-3">
             <p className="col-span-2 text-xl font-semibold ">Weaknesses</p>
@@ -88,7 +88,7 @@ const Home: NextPage = () => {
           <div className="space-y-8">
             <div className="space-y-2">
               <h2 className="text-3xl font-semibold">Summary</h2>
-              <p className="font-lustria sm:text-lg text-base">{summary}</p>
+              <p className="font-lustria text-base sm:text-lg">{summary}</p>
             </div>
             <div className="space-y-2">
               <h2 className="text-3xl font-semibold">Bestiary Entry</h2>
@@ -103,7 +103,11 @@ const Home: NextPage = () => {
               <h2 className="text-3xl font-semibold">Combat Tactics</h2>
               <ul className="space-y-2">
                 {combatTactics.map((entry, entryIdx) => {
-                  return <li className="font-lustria text-md sm:text-base" key={entryIdx}>{entry}</li>;
+                  return (
+                    <li className="font-lustria text-base sm:text-lg" key={entryIdx}>
+                      {entry}
+                    </li>
+                  );
                 })}
               </ul>
             </div>
@@ -115,6 +119,58 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data, error } = await supabase.from("creatures").select(`
+    id,
+    name
+  `);
+  if (error) {
+    console.error(error);
+  }
+  if (data) {
+    const paths = data.map((creature) => {
+      return { params: { creatureName: creature.name } };
+    });
+    return { paths, fallback: false };
+  }
+  return { paths: [{ params: { creatureName: "" } }], fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const currentCreatureName = context.params?.creatureName;
+  const { data, error } = await supabase
+    .from("creatures")
+    .select(
+      `
+        id,
+        name,
+        type,
+        weaknesses,
+        images->images,
+        short_description,
+        summary,
+        bestiary_entry->bestiary_entry,
+        combat_tactics->combat_tactics
+      `
+    )
+    .eq("name", currentCreatureName)
+    .single();
+
+  return {
+    props: {
+      id: data?.id,
+      name: data?.name,
+      type: data?.type,
+      weaknesses: data?.weaknesses.split(","),
+      images: [...data?.images],
+      shortDescription: data?.short_description,
+      summary: data?.summary,
+      bestiaryEntry: data?.bestiary_entry,
+      combatTactics: data?.combat_tactics,
+    },
+  };
+};
 
 const cardDetil: IDetailPage = {
   id: "1",
