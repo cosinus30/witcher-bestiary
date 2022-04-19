4f04e73b8b764827438f7fe55129691331b8ffb3
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-04-19 22:25:53 +0300

    style: reduce the max width of deck to increase UX

--- components/CardDetail/ImageDeck.tsx
+++ components/CardDetail/ImageDeck.tsx
@@ -9,15 +9,17 @@ interface IImageDeck {
 
 const ImageDeck = ({ images }: IImageDeck) => {
   return (
-    <div className="grid grid-flow-col gap-2 sm:grid-rows-2">
-      <div className="overflow-hidden row-span-2 w-full rounded-sm shadow-lg aspect-w-9 aspect-h-16">
-        <Image src={images[0].url} alt={images[0].alt} layout="fill" objectFit="cover"></Image>
-      </div>
-      <div className="hidden overflow-hidden col-span-1 w-full rounded-sm shadow-lg aspect-w-4 aspect-h-3 sm:block">
-        <Image src={images[1].url} alt={images[1].alt} layout="fill" objectFit="cover"></Image>
-      </div>
-      <div className="hidden overflow-hidden col-span-1 w-full rounded-sm shadow-lg aspect-w-4 aspect-h-3 sm:block">
-        <Image src={images[2].url} alt={images[2].alt} layout="fill" objectFit="cover"></Image>
+    <div className="mx-auto max-w-2xl">
+      <div className="grid grid-flow-col gap-2 sm:grid-rows-2">
+        <div className="overflow-hidden row-span-2 w-full rounded-sm shadow-lg aspect-w-9 aspect-h-16">
+          <Image src={images[0].url} alt={images[0].alt} layout="fill" objectFit="cover"></Image>
+        </div>
+        <div className="hidden overflow-hidden col-span-1 w-full rounded-sm shadow-lg aspect-w-4 aspect-h-3 sm:block">
+          <Image src={images[1].url} alt={images[1].alt} layout="fill" objectFit="cover"></Image>
+        </div>
+        <div className="hidden overflow-hidden col-span-1 w-full rounded-sm shadow-lg aspect-w-4 aspect-h-3 sm:block">
+          <Image src={images[2].url} alt={images[2].alt} layout="fill" objectFit="cover"></Image>
+        </div>
       </div>
     </div>
   );
