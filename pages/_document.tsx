e6345055d816bc9d87add657afb282ea2f428fab
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-04-19 23:35:30 +0300

    fix: type error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,31 +5,21 @@ import Document, { Html, Head, Main, NextScript, DocumentInitialProps, DocumentC
 // https://nextjs.org/docs/advanced-features/custom-document
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () => originalRenderPage();
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-      };
-    } catch (err) {
-      console.error(err);
-    }
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
   }
 
   render(): ReactElement {
     return (
       <Html lang="en">
         <Head>
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
-            <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;600;700&display=swap" rel="stylesheet"/>
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
-            <link href="https://fonts.googleapis.com/css2?family=Lustria&display=swap" rel="stylesheet"/>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+          <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;600;700&display=swap" rel="stylesheet" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+          <link href="https://fonts.googleapis.com/css2?family=Lustria&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
