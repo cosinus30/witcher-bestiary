d755fee8d903d4c6c7a5f8316c29dc9f1eb45354
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-04-19 22:36:02 +0300

    fix: name exceeding one line

--- components/HomePage/Card.tsx
+++ components/HomePage/Card.tsx
@@ -26,7 +26,7 @@ const Card = ({ type, image, name, weaknesses }: CardProps) => {
           ></Image>
         </div>
           <div className="flex absolute inset-x-0 bottom-0 justify-center items-center text-white bg-darky shadow-inner drop-shadow-lg">
-            <span>{name}</span>
+            <p className="truncate">{name}</p>
             <span className="pt-1 ml-2">
               <Image src={`/${type}.svg`} alt={type} width={32} height={32}></Image>
             </span>
