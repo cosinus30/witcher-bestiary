5f1a641266d38e02b9d8d27c42ca90f6ee6f22bb
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-04-23 21:06:38 +0300

    fix: reduce main bar width to improve UX

--- components/HomePage/MainBar.tsx
+++ components/HomePage/MainBar.tsx
@@ -3,9 +3,9 @@ import Filter from "./Filter";
 
 const MainBar = () => {
   return (
-    <div className="flex flex-col justify-center p-4 my-8 mx-auto border border-secondary/75 md:flex-row md:justify-between md:items-center md:max-w-5xl">
+    <div className="flex flex-col justify-center p-4 my-8 mx-auto border border-secondary/75 md:flex-row md:justify-between md:items-center md:max-w-4xl">
       <Filter />
-      <Dropdown /> 
+      <Dropdown />
     </div>
   );
 };
