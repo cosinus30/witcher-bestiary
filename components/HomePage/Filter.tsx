d565e3fd6bb47d8420d35a83c5f24175746069d6
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-04-23 21:07:27 +0300

    fix: overflowing of filters in mobile

--- components/HomePage/Filter.tsx
+++ components/HomePage/Filter.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { FilterContext, FilterContextType } from "../../context/FilterProvider";
 
-
 const Filter = () => {
-  const { filters, updateFilters} = React.useContext(FilterContext) as FilterContextType;
+  const { filters, updateFilters } = React.useContext(FilterContext) as FilterContextType;
 
   return (
-    <div className="flex items-center space-x-4 md:flex-col">
+    <div className="flex flex-col items-center space-x-4 md:flex-row">
       <h1 className="text-xl font-bold text-white">Weaknesses</h1>
       <ul className="flex space-x-2">
         {filters.map(({ icon, label, value, selected }) => {
           return (
             <li key={value}>
-              <button className={`flex flex-col ${selected ? "opacity-100" : "opacity-30"} hover:opacity-100`} onClick={() => updateFilters(value)}>
-                {icon && <span className="m-0">{icon}</span>}
-                <span className="text-xs text-white">{label}</span>
+              <button
+                className={`flex flex-col mx-1 ${selected ? "opacity-100" : "opacity-30"} hover:opacity-100`}
+                onClick={() => updateFilters(value)}
+              >
+                {icon && <span className="mx-auto">{icon}</span>}
+                <p className="text-xs text-center">{label}</p>
               </button>
             </li>
           );
