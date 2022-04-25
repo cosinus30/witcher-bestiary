36d71d7db68f7d396c02727eb68c0bcf7a22356a
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-04-25 17:30:05 +0300

    fix: hovering UX issue Increase opacity to amplify card is active Show cursor on hover

--- components/HomePage/Card.tsx
+++ components/HomePage/Card.tsx
@@ -16,14 +16,14 @@ const Card = ({ type, image, name }: CardProps) => {
   return (
     // eslint-disable-next-line @next/next/link-passhref
     <Link href={`/${name}`}>
-      <div className="group relative transition-all duration-300 sm:hover:scale-110">
+      <div className="group relative transition-all duration-300 hover:cursor-pointer sm:hover:scale-110">
         <div className="overflow-hidden w-full rounded-sm aspect-w-3 aspect-h-4">
           <Image
             src={image.url}
             alt={image.alt}
             layout="fill"
             objectFit="contain"
-            className="group-hover:opacity-75"
+            className="opacity-75 group-hover:opacity-100"
           ></Image>
         </div>
           <div className="flex absolute inset-x-0 bottom-0 justify-center items-center text-white bg-darky shadow-inner drop-shadow-lg">
