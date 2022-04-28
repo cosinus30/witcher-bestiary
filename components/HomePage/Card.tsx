54f22afb88458d7537df3b4a185562e5547f6dd5
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-04-28 18:01:19 +0300

    feat: add animation for entering-exiting of cards

--- components/HomePage/Card.tsx
+++ components/HomePage/Card.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 export interface CardProps {
   image: {
@@ -15,25 +16,27 @@ export interface CardProps {
 const Card = ({ type, image, name }: CardProps) => {
   return (
     // eslint-disable-next-line @next/next/link-passhref
-    <Link href={`/${name}`}>
-      <div className="group relative transition-all duration-300 hover:cursor-pointer sm:hover:scale-110">
-        <div className="overflow-hidden w-full rounded-sm aspect-w-3 aspect-h-4">
-          <Image
-            src={image.url}
-            alt={image.alt}
-            layout="fill"
-            objectFit="contain"
-            className="opacity-75 group-hover:opacity-100"
-          ></Image>
-        </div>
+    <motion.div layout animate={{ opacity: 1 }} initial={{ opacity: 0 }} exit={{ opacity: 0 }}>
+      <Link href={`/${name}`}>
+        <div className="group relative transition-all duration-300 hover:cursor-pointer sm:hover:scale-110">
+          <div className="overflow-hidden w-full rounded-sm aspect-w-3 aspect-h-4">
+            <Image
+              src={image.url}
+              alt={image.alt}
+              layout="fill"
+              objectFit="contain"
+              className="opacity-75 group-hover:opacity-100"
+            ></Image>
+          </div>
           <div className="flex absolute inset-x-0 bottom-0 justify-center items-center text-white bg-darky shadow-inner drop-shadow-lg">
             <p className="truncate">{name}</p>
             <span className="pt-1 mx-3 ">
               <Image src={`/${type}.svg`} alt={type} width={24} height={24}></Image>
             </span>
           </div>
-      </div>
-    </Link>
+        </div>
+      </Link>
+    </motion.div>
   );
 };
 
--- components/HomePage/Cards.tsx
+++ components/HomePage/Cards.tsx
@@ -1,16 +1,21 @@
 import Card, { CardProps } from "./Card";
-
+import { motion, AnimatePresence } from "framer-motion";
 export interface CardsProps {
   cards: CardProps[];
 }
 
 const Cards = ({ cards }: CardsProps) => {
   return (
-    <div className="grid grid-cols-2 mx-auto space-y-8 space-x-6 max-w-5xl sm:grid-cols-3 md:grid-cols-5">
-      {cards.map((card) => {
-        return <Card key={card.id} {...card} />;
-      })}
-    </div>
+    <motion.div
+      layout
+      className="grid grid-cols-2 mx-auto space-y-8 space-x-6 max-w-5xl sm:grid-cols-3 md:grid-cols-5"
+    >
+      <AnimatePresence>
+        {cards.map((card) => {
+          return <Card key={card.id} {...card} />;
+        })}
+      </AnimatePresence>
+    </motion.div>
   );
 };
 
